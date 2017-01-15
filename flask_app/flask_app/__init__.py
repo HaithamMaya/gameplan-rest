cda61c8f0d763c2acf87b41e75e72cb6b696ad0f
cashc <cash@cashc.me>
2017-01-15 13:34:52 -0500

    fixed config file usage.

--- flask_app/flask_app/__init__.py
+++ flask_app/flask_app/__init__.py
@@ -7,22 +7,21 @@ import os
 app = Flask(__name__)
 
 script = os.getcwd().split('/')[-1]
-print('running: ',script)
 
-if script == 'flask_tests':
-    app.config['DEBUG'] = True
-    app.config['TESTING'] = True
-    app.config.from_pyfile('test_config/test_config_db.py')
-    app.config.from_pyfile('test_config/test_config_security.py')
-    app.config.from_pyfile('test_config/test_config_mail.py')
-    app.config.from_pyfile('test_config/test_config_swagger.py')
-else:
+if script == 'flask_app':
     app.config['DEBUG'] = True
     app.config['TESTING'] = False
     app.config.from_pyfile('config/config_db.py')
     app.config.from_pyfile('config/config_security.py')
     app.config.from_pyfile('config/config_mail.py')
     app.config.from_pyfile('config/config_swagger.py')
+else: # gameplan-rest or flask_tests
+    app.config['DEBUG'] = True
+    app.config['TESTING'] = True
+    app.config.from_pyfile('test_config/test_config_db.py')
+    app.config.from_pyfile('test_config/test_config_security.py')
+    app.config.from_pyfile('test_config/test_config_mail.py')
+    app.config.from_pyfile('test_config/test_config_swagger.py')
 
 db = SQLAlchemy(app)
 oauth = OAuth2Provider(app)
